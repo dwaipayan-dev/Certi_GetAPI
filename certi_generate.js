be38217ff2e574eeeb537fdf23192df0089c1a94
dwaipayan-dev <dwaipayan.vidyanta1998@gmail.com>
2021-07-11 22:56:21 +0530

    second commit

---
+++ certi_generate.js
@@ -0,0 +1,23 @@
+const Jimp = require('jimp');
+
+async function addText(){
+    const image = await Jimp.read('./assets/Test_png.png'); 
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+    image.print(font, 0,0,{
+        text:"This is to certify that You have completed this course..",
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+    }, 1920, 1080);
+    await image.writeAsync('Received.png');
+    return image;
+}
+
+addText().then((image)=>{
+    image.getBuffer(Jimp.MIME_PNG, (err, data)=>{
+        if (err) return console.error(err);
+        console.log("Function successfully returns");
+        res.contentType('image/png');
+        res.send(data);
+    });
+    
+});
--- server.js
+++ server.js
@@ -7,9 +7,33 @@ const PORT = 8080;
 
 const connectDb = require('./src/connection');
 const User = require('./src/User.model');
+const fs = require('fs');
+const Jimp = require('jimp');
+
 //Route 1
 app.get("/", (req, res)=>{
-    res.send("Hello from Node.js app \n");
+    //res.send("Hello from Node.js app \n");
+    async function addText(){
+        const image = await Jimp.read('./assets/Test_png.png'); 
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+        image.print(font, 0,0,{
+            text:"This is to certify that You have completed this course..",
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+        }, 1920, 1080);
+        await image.writeAsync('Received.png');
+        return image;
+    }
+    addText().then((image)=>{
+        image.getBuffer(Jimp.MIME_PNG, (err, data)=>{
+            if (err) return console.error(err);
+            console.log("Function successfully returns");
+            res.contentType('image/png');
+            res.send(data);
+        });
+        
+    });
+    
 });
 
 app.get("/user-create", (req, res)=>{
